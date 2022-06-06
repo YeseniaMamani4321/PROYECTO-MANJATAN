b94b67a0a156497db1c494deaa9c61351b0d72b6
Yessenia Mamani <yesseniamamani4321@gmail.com>
2022-06-06 10:56:51 -0400

    prueba de git remoto

--- src/proyecto/modelo/BaseDeDatos.java
+++ src/proyecto/modelo/BaseDeDatos.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto.modelo;
-//aqui se mejoro algo 
+//aqui se mejoro
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
